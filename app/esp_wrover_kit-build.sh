13c47d4f82778f7615e848ee337777250833c798
Maximilian Huber <gh@maxhbr.de>
2024-01-21 10:08:44 +0100

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- app/esp_wrover_kit-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")"
-mkdir -p builds
-set -x
-west build \
-    -d builds/esp_wrover_kit \
-    -b esp_wrover_kit \
-    .
--- app/native_posix_64-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")"
-mkdir -p builds
-west build \
-    -d builds/native_posix_64 \
-    -b native_posix_64 \
-    .
