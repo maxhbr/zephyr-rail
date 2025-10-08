c8e35db387fd80c6e1067f677c3f731bf1a143e6
Maximilian Huber <gh@maxhbr.de>
2025-10-08 20:16:45 +0200

    add documentation
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- app/native_run.build_and_run.sh
+++ app/native_run.build_and_run.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 # or native_sim/native/64
-build_dir="native_sim.build"
+build_dir="builds/native_sim"
+mkdir -p "$build_dir"
 west build -b native_sim . --build-dir "$build_dir"
 "$build_dir/zephyr/zephyr.exe" --bt-dev=127.0.0.1:9000
