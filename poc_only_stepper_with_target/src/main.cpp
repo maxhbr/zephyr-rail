dc718e93f4921b3635e568afad9d79302b74c2bd
Maximilian Huber <gh@maxhbr.de>
2025-10-14 21:33:55 +0200

    wip
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- poc_only_stepper_with_target/src/main.cpp
+++ poc_only_stepper_with_target/src/main.cpp
@@ -17,22 +17,20 @@ int main(void) {
 
   LOG_INF("Stepper device is ready");
 
-  // Enable the stepper motor
   int ret = stepper_enable(stepper_dev);
   if (ret < 0) {
     LOG_ERR("Failed to enable stepper: %d", ret);
     return ret;
   }
 
-  // Set micro stepping resolution (if supported)
-  ret = stepper_set_micro_step_res(stepper_dev, STEPPER_MICRO_STEP_16);
-  if (ret < 0) {
-    LOG_WRN("Failed to set micro step resolution: %d", ret);
-  }
+  //   // Set micro stepping resolution (if supported)
+  //   ret = stepper_set_micro_step_res(stepper_dev, STEPPER_MICRO_STEP_16);
+  //   if (ret < 0) {
+  //     LOG_WRN("Failed to set micro step resolution: %d", ret);
+  //   }
 
-  // Set step interval (speed control)
-  ret =
-      stepper_set_microstep_interval(stepper_dev, 1000000); // 1ms between steps
+  ret = stepper_set_microstep_interval(stepper_dev,
+                                       20000000); // 20ms between steps
   if (ret < 0) {
     LOG_WRN("Failed to set step interval: %d", ret);
   }
@@ -40,7 +38,6 @@ int main(void) {
   LOG_INF("Testing stepper motor movements...");
 
   while (1) {
-    // Move 200 steps clockwise (relative movement)
     LOG_INF("Moving 200 steps clockwise");
     ret = stepper_move_by(stepper_dev, 200);
     if (ret < 0) {
@@ -49,7 +46,6 @@ int main(void) {
 
     k_sleep(K_SECONDS(2));
 
-    // Move 200 steps counter-clockwise (relative movement)
     LOG_INF("Moving 200 steps counter-clockwise");
     ret = stepper_move_by(stepper_dev, -200);
     if (ret < 0) {
