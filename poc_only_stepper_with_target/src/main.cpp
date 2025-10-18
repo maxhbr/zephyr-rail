ca3e73fbe995a516819ff4fe996bd9be3fc4f0cf
Maximilian Huber <gh@maxhbr.de>
2025-10-18 12:33:02 +0200

    wip
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- poc_only_stepper_with_target/src/main.cpp
+++ poc_only_stepper_with_target/src/main.cpp
@@ -1,4 +1,5 @@
 #include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/stepper.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -6,6 +7,7 @@
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #define STEPPER_NODE DT_NODELABEL(stepper_motor)
+#define LED0_NODE DT_ALIAS(led0)
 
 // Callback for stepper events
 static volatile bool movement_complete = false;
@@ -40,9 +42,24 @@ int main(void) {
 
   LOG_INF("Stepper device is ready");
 
+  // Set up LED
+  static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+  if (!gpio_is_ready_dt(&led)) {
+    LOG_ERR("LED device is not ready");
+    return -1;
+  }
+
+  int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+  if (ret < 0) {
+    LOG_ERR("Failed to configure LED: %d", ret);
+    return ret;
+  }
+
+  LOG_INF("LED is ready");
+
   // Set up event callback
-  int ret =
-      stepper_set_event_callback(stepper_dev, stepper_event_callback, NULL);
+  ret = stepper_set_event_callback(stepper_dev, stepper_event_callback, NULL);
   if (ret < 0) {
     LOG_WRN("Failed to set event callback: %d", ret);
   }
@@ -65,8 +82,9 @@ int main(void) {
   LOG_INF("Testing stepper motor movements...");
 
   while (1) {
-    // Move clockwise
+    // Move clockwise (forward direction - LED ON)
     LOG_INF("Moving 200 steps clockwise");
+    gpio_pin_set_dt(&led, 1); // Turn LED on for forward direction
     movement_complete = false;
     ret = stepper_move_by(stepper_dev, 200);
     if (ret < 0) {
@@ -83,8 +101,9 @@ int main(void) {
     LOG_INF("Clockwise movement finished, pausing...");
     k_sleep(K_SECONDS(1));
 
-    // Move counter-clockwise
+    // Move counter-clockwise (reverse direction - LED OFF)
     LOG_INF("Moving 200 steps counter-clockwise");
+    gpio_pin_set_dt(&led, 0); // Turn LED off for reverse direction
     movement_complete = false;
     ret = stepper_move_by(stepper_dev, -200);
     if (ret < 0) {
